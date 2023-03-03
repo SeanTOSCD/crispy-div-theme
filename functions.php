e6079aac7249798c4e7b36ce1780dc24db7c9a29
Sean Davis <sdavis2702@gmail.com>
2023-03-03 07:06:06 -0600

    version bump 1.0.27

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
  * Crispy Div functions and definitions
  */
 
-const THEME_VERSION = '1.0.26';
+const THEME_VERSION = '1.0.27';
 
 define( 'THEME_URI', get_home_url() );
 define( 'THEME_NAME', get_bloginfo( 'name' ) );
