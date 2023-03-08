52452abf6112746de3f6093cbe96e5a1ff2e7eb7
Sean Davis <sdavis2702@gmail.com>
2023-03-08 09:32:11 -0600

    enhance the Brand Assets page

--- page-brand-assets.php
+++ page-brand-assets.php
@@ -14,7 +14,9 @@ crispydiv_page_header( array(
 	<main id="site-content" class="site-main">
 		<div class="inner medium">
             <h3 class="brand-asset-title">Brand Colors</h3>
-            <p>Feel free to use these colors responsibly to ensure a cohesive look and feel.</p>
+            <div class="section-description">
+                <p>Please use these colors responsibly to ensure a cohesive look and feel. If you'd like to keep it simple, our two primary colors are purple <strong>(#341671)</strong> and orange <strong>(#f54952)</strong>.</p>
+            </div>
             <div class="colors-grid">
                 <div class="color-1">
                     <span class="hex">#422680</span>
@@ -42,7 +44,9 @@ crispydiv_page_header( array(
                 </div>
             </div>
             <h3 class="brand-asset-title">Logos & Logomark</h3>
-            <p>Keep contrast in mind when implementing the logo. Always give the images room to breathe. Thanks!</p>
+            <div class="section-description">
+                <p>Try to keep contrast in mind when implementing the logo so that the text is legible. Also be sure to give the logo room to breathe. Do not allow it to touch or overlap any other elements (subtle background design elements are okay).</p>
+            </div>
             <div class="logos-grid">
                 <div class="logo-white background-purple">
                     <img src="<?php echo THEME_ASSETS . 'brand/crispy-div-logo-white.svg'; ?>" alt="Crispy Div Logo - White">
@@ -57,8 +61,28 @@ crispydiv_page_header( array(
                     <img src="<?php echo THEME_ASSETS . 'brand/crispy-div-logo-mark.svg'; ?>" alt="Crispy Div Logomark">
                 </div>
             </div>
+            <h3 class="brand-asset-title">Typography & Text</h3>
+            <div class="section-description">
+                <p>You have no obligation to use our preferred font when referencing our brand. If you choose to do so, we use <a href="https://fonts.google.com/specimen/Montserrat?query=montse" target="_blank" rel="nofollow">Montserrat</a> for headings (font weights 700 and 800) and <a href="https://fonts.google.com/specimen/Raleway?query=ralew" target="_blank" rel="nofollow">Raleway</a> for body text. Our Brand name is <strong>Crispy Div</strong>. That's capital C and D with a space between the two words.</p>
+            </div>
+            <div class="brand-name-grid">
+                <div class="brand-name-example background-gray incorrect">
+                    <span class="brand-name"><i class="fa-solid fa-circle-xmark"></i>CrispyDiv</span>
+                </div>
+                <div class="brand-name-example background-gray incorrect">
+                    <span class="brand-name"><i class="fa-solid fa-circle-xmark"></i>crispydiv</span>
+                </div>
+                <div class="brand-name-example background-gray incorrect">
+                    <span class="brand-name"><i class="fa-solid fa-circle-xmark"></i>crispy div</span>
+                </div>
+                <div class="brand-name-example background-gray correct">
+                    <span class="brand-name"><i class="fa-solid fa-circle-check"></i>Crispy Div</span>
+                </div>
+            </div>
             <h3 class="brand-asset-title">Downloads</h3>
-            <p>All logo and logomark variations can be downloaded to your machine in a single .zip file.</p>
+            <div class="section-description">
+                <p>Logo and logomark variations can be downloaded in a single .zip file.</p>
+            </div>
             <div class="download-logos">
                 <a href="<?php echo THEME_ASSETS . 'brand/crispy-div-brand-assets.zip'; ?>" download="crispy-div-brand-assets.zip" class="button button-purple">Download All Assets</a>
             </div>
