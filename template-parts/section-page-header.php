2ac340f3dcdfe27d8de7ebb53cc35dca67864c57
Sean Davis <sdavis2702@gmail.com>
2023-03-09 11:20:39 -0600

    make sure only single posts get centered styles

--- template-parts/section-page-header.php
+++ template-parts/section-page-header.php
@@ -37,7 +37,7 @@ if ( ! empty( $args['corner-accent-color'] ) ) {
             <?php } ?>
             <?php echo $args['title']; ?>
         </h1>
-		<?php if ( is_single() ) { ?>
+		<?php if ( is_singular( 'post' ) ) { ?>
             <div class="entry-meta">
                 <span class="posted-on"><?php echo crispydiv_posted_on(), crispydiv_posted_by(); ?></span>
             </div>
